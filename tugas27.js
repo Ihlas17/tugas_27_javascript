76a263741526bea510d8ceb618afe35dc29b5932
ihlas_sul <ihlassmada@gmail.com>
2021-11-04 10:42:58 +0800

    tugas 27 javascript
    done

---
+++ tugas27.js
@@ -0,0 +1,15 @@
+let temp1 = ''
+let kalimat1 = ["No","rumah saya",30, "A"]
+kalimat1.forEach(function(a){
+    temp1+=a
+    temp1+=' '
+})
+console.log(temp1)
+
+let temp2 = ''
+let kalimat2 = ["Jalan pangeran Tirtayasa No", 70]
+kalimat2.forEach(function(a){
+    temp2+=a
+    temp2+=' '
+})
+console.log(temp2)
